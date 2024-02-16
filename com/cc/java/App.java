5126f51feb3050ec79cdc4608cccd854e38549a7
DPSanto95 <160004936+DPSanto95@users.noreply.github.com>
2024-02-16 11:58:11 +0000

    Variablen deklarieren

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -2,10 +2,20 @@ package com.cc.java;
 
 public class App {
     
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
 
-        System.out.println("Hello, World!");
-       
+        // It is what it is
+
+            String food = "leer";
+            String drink = "leer";
+
+        System.out.println("Hi Daniel");
+        System.out.println(food);
+       System.out.println(drink);
+
+
+
     }
 }
 
