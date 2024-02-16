a1a4e92f96d13b77145d37a820bc454e492f62aa
DPSanto95 <160004936+DPSanto95@users.noreply.github.com>
2024-02-16 15:28:39 +0000

    Bestellung getränk+Burger

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -1,21 +1,57 @@
 package com.cc.java;
 
-public class App {
+public class App 
     
     public static void main(String[] args) 
     {
-
-        // It is what it is
-
-            String food = "leer";
-            String drink = "leer";
-
-        System.out.println("Hi Daniel");
+        
+        // Variablen deklarieren
+        String food = "noch kein Burger gewählt";
+        String drink = "noch kein Getränk gewählt";
+        String itemNumStr;
+        String flag;
+        System.out.println(food);  
+        System.out.println(drink); 
+        // Bestelldialog 1 : Ausgabe
+        System.out.println("-------------------------------");
+        System.out.println("Guten Tag bei MD, Ihre Bestellung bitte!");
+        System.out.println("Hamburger(1), Cheeseburger(2) oder Chilliburger(3) ?");
+        // Bestelldialog 1 : Eingabe
+        itemNumStr = System.console().readLine("Ihre Wahl? : ");
+        System.out.println(itemNumStr);  // 1 oder 2 oder 3
+        // Zuordnung Zahl --> Burger
+        switch (itemNumStr)
+        {
+            case "1":
+                food = "Hamburger";
+                break;
+            case "2":
+                food = "Cheeseburger";
+                break;
+            case "3":
+                food = "Chilliburger";
+                break;
+        }
         System.out.println(food);
-       System.out.println(drink);
-
-
-
-    }
-}
-
+        // Entscheidung Getränk dazu?
+        System.out.println("Möchten Sie ein Getränk dazu bestellen? (j/n)");
+        flag = System.console().readLine("(j/n)? :");
+        System.out.println(flag);
+      	 // Getränkeasuwahl
+        if (flag.equals("j")) 
+            System.out.println("Cola(1) , Fanta(2), Sprite(3)?");
+           itemNumStr = System.console().readLine("Ihre Wahl? : ");
+            switch (itemNumStr)
+            {
+                case "1":
+                    drink = "Cola" ;
+                    break;
+                case "2":
+                    drink = "Fanta";
+                    break;
+                case "3":
+            
+                    drink = "Sprite" ;
+                    break;
+            }
+        }
